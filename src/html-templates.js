10f1eeae657008ac1790f0b8816d68b747c2305d
Jordan Schneringer <schneringerj@gmail.com>
2022-06-23 23:19:08 -0700

    added front-end HTML

---
+++ src/html-templates.js
@@ -0,0 +1,25 @@
+const makeTeam = team => {
+
+    const generateManagerCard = manager => {
+        return `
+        <div class="card" style="width: 18rem">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+      </div>
+      <ul class="list-group list-group-flush">
+        <!--Added test email -->
+        <a href="mailto:admin@uclaextension.edu">
+          <li class="list-group-item">An item</li></a
+        >
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+      </ul>
+        `
+        
+        
+    }
+
+
+
+
+}
